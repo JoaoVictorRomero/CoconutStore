7156a777054e2ad9726e678eb64d35dfcc69df06
JoaoVictorRomero <halleyloup-study@outlook.com>
2025-06-22 21:50:40 -0300

    Padronizado largura de 1280

--- src/app/_components/navbar.tsx
+++ src/app/_components/navbar.tsx
@@ -2,22 +2,24 @@
 
 export function Navbar() {
   return(
-    <nav className="flex flex-row h-12 bg-orange-50 font-sans">
-      <ul className="basis-1/3 flex justify-center items-center">
-        <li className="text-xl font-bold"><a href="#" > CoconutStore </a></li>
-      </ul>
+    <div className="bg-orange-50 font-sans">
+      <nav className="flex h-12  max-w-[1280px] m-auto">
+        <ul className="basis-1/3 flex justify-start items-center">
+          <li className="text-xl text-amber-900 font-bold"><a href="#" > 🥥 CoconutStore </a></li>
+        </ul>
 
-      <ul className="basis-1/3 flex justify-center gap-8 items-center text-sm">
-        <li><a href="#"> Início</a></li>
-        <li><a href="#"> Produtos</a></li>
-        <li><a href="#"> Sobre</a></li>
-        <li><a href="#"> Contato</a></li>
-      </ul>
+        <ul className="basis-1/3 flex justify-center gap-8 items-center text-sm">
+          <li><a href="#"> Início</a></li>
+          <li><a href="#"> Produtos</a></li>
+          <li><a href="#"> Sobre</a></li>
+          <li><a href="#"> Contato</a></li>
+        </ul>
 
 
-      <ul className="basis-1/3 flex justify-center items-center">
-        <li><a href="#"> Carrinho</a></li>
-      </ul>
-    </nav>
+        <ul className="basis-1/3 flex justify-end items-center">
+          <li><a href="#"> 🛒 Carrinho</a></li>
+        </ul>
+      </nav>
+    </div>
   )
 }
