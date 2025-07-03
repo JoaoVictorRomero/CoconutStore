4d982e1fa0e8e06bdbcb1ae7a2d6059659bebf21
balder <gabrielbalder@gmail.com>
2025-07-03 11:41:00 -0300

    Alteração cor h2

--- src/app/_components/ProductShowcase.tsx
+++ src/app/_components/ProductShowcase.tsx
@@ -2,7 +2,7 @@ export function ProductShowcase() {
   return (
     <section className="bg-gradient-to-r from-[#FFF9E6] to-[#F1FDF4] font-sans py-12 px-4">
       <div className="max-w-[1280px] mx-auto flex flex-col items-center">
-        <h2 className="text-3xl md:text-4xl text-amber-900 font-bold mb-4 text-center">
+        <h2 className="text-3xl md:text-4xl text-black-900 font-bold mb-4 text-center">
           Conheça Nossos Produtos
         </h2>
         <p className="text-gray-600 text-lg max-w-[600px] text-center">
