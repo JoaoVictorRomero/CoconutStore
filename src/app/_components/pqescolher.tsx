8eb0800037150cd1e07307911c0a0c616052a192
bernardoleinig <bernardoleinig@gmail.com>
2025-07-12 14:55:46 -0300

    adicionado o componente pqescolher

--- src/app/_components/pqescolher.tsx
+++ src/app/_components/pqescolher.tsx
@@ -8,12 +8,12 @@ export function Pqescolher(){
 
     return(  
 
-        <div className="max-w-[1280px] h-[492px] bg-[linear-gradient(14deg,_#F0FDF4,_#FFFBEB)]">
+        <div className="max-w-[1280px] h-[460px] bg-[linear-gradient(14deg,_#F0FDF4,_#FFFBEB)]">
             
         {/* Bloco do Titulo e Subtítulo */}
             <div className=" w-[600px] m-auto pt-15 pb-15">
                 <h1 className="text-center text-[35px] font-semibold">Por que Escolher a CoconutStore?</h1>
-                <p className="text-center text-[#4B5563] p-3">Compromisso com a qualidade e satisfação do cliente</p>
+                <p className="text-center text-[#4B5563] p-3 text-[18.5px]">Compromisso com a qualidade e satisfação do cliente</p>
             </div>
         
         
@@ -22,11 +22,11 @@ export function Pqescolher(){
             {/* Bloco do 100% Natural*/}
                 <div className="w-[384px]">
                     
-                    <div className="bg-white rounded-full w-[50px] h-[50px] m-auto shadow-lg/30">
+                    <div className="transition-transform duration-300 ease-in-out hover:scale-115 bg-white rounded-full w-[50px] h-[50px] m-auto shadow-lg/30">
                          <img className = "m-auto p-3"src="/imagem_componente_pqescolher1.png" alt="imagem de uma folha" />
                     </div>
                    
-                    <h3 className="text-center font-semibold p-4"> 100% Natural </h3>
+                    <h3 className="text-center font-bold p-4"> 100% Natural </h3>
                     <p className= "text-center text-[#4B5563] text-[13px]"> Cocos cultivados sem agrotóxicos, direto da fazenda para sua mesa </p>
                     
                 </div>
@@ -34,12 +34,12 @@ export function Pqescolher(){
             {/* Bloco da Qualidade Garantida*/}
                 <div className="w-[384px]">
 
-                    <div className="bg-white rounded-full w-[50px] h-[50px] m-auto shadow-lg/30">
+                    <div className=" transition-transform duration-300 ease-in-out hover:scale-115 bg-white rounded-full w-[50px] h-[50px] m-auto shadow-lg/30">
                          <img className="m-auto p-3" src="imagem_componente_pqescolher2.png" alt="" />
                     </div>
 
                     
-                    <h3 className="text-center font-semibold p-4"> Qualidade Garantida </h3>
+                    <h3 className="text-center font-bold p-4"> Qualidade Garantida </h3>
                     <p className="text-center text-[#4B5563] text-[13px] "> Rigoroso controle de qualidade em todas as etapas do processo </p>
 
                 </div>
@@ -47,12 +47,12 @@ export function Pqescolher(){
             {/* Bloco da Entrega Rápida*/}
                 <div className="w-[384px]">
                     
-                    <div className="bg-white rounded-full w-[50px] h-[50px] m-auto shadow-lg/30">
+                    <div className="transition-transform duration-300 ease-in-out hover:scale-115 bg-white rounded-full w-[50px] h-[50px] m-auto shadow-lg/30">
                          <img className="m-auto p-3" src="imagem_componente_pqescolher3.png" alt="" />
                     </div>
 
                     
-                    <h3 className="text-center font-semibold p-4"> Entrega Rápida </h3>
+                    <h3 className="text-center font-bold p-4"> Entrega Rápida </h3>
                     <p className="text-center text-[#4B5563] text-[13px]"> Entregamos em até 24h para manter o frescor do produto </p>
                 
                 </div>
