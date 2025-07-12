3563ad9c02c598dd5630853ccea06ef52705084c
JoaoVictorRomero <halleyloup-study@outlook.com>
2025-07-12 17:52:04 -0300

    Navbar com component Button

--- src/app/_components/navbar.tsx
+++ src/app/_components/navbar.tsx
@@ -1,4 +1,12 @@
+import { Button } from "src/components/ui/button"
 
+function ShoppingCart() {
+  return(
+    <>
+      <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-shopping-cart"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M6 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" /><path d="M17 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" /><path d="M17 17h-11v-14h-2" /><path d="M6 5l14 1l-1 7h-13" /></svg>
+    </>
+  )
+}
 
 export function Navbar() {
   return(
@@ -17,7 +25,10 @@ export function Navbar() {
 
 
         <ul className="basis-1/3 flex justify-end items-center">
-          <li><a href="#"> 🛒 Carrinho</a></li>
+          <li><Button variant="outline" size="sm" >
+            <ShoppingCart/>
+            Carrinho
+          </Button></li>
         </ul>
       </nav>
     </div>
