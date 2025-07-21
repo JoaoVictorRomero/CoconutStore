2485a923317ed1ab3f5b49f854cff686911330f0
JoaoVictorRomero <halleyloup-study@outlook.com>
2025-07-21 11:18:13 -0300

    Procedure e client api para achar produtos funcionando

--- src/app/produtos/page.tsx
+++ src/app/produtos/page.tsx
@@ -1,9 +1,18 @@
+"use client"
+
+import { api } from "~/trpc/react";
 import { CardProduto } from "../_components/cardProduto"
 
 export default function Page() {
+  const [sla] = api.user.showProduct.useSuspenseQuery();
+
+
   return(
     <div className="max-w-[1280px] m-auto pt-6 px-4">
       <div className="grid grid-cols-[repeat(auto-fit,minmax(300px,1fr))]  gap-4 place-content-center ">
+        {sla.map((product) => (
+          <p>{product.name}</p>
+        ))}
         <CardProduto />
         <CardProduto />
         <CardProduto />
