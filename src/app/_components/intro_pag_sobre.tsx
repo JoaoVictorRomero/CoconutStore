147040162bc9260877e2f8555eeab9819d5937a6
bernardoleinig <bernardoleinig@gmail.com>
2025-07-21 10:55:58 -0300

    criado o componente - sobre a coconutstore

---
+++ src/app/_components/intro_pag_sobre.tsx
@@ -0,0 +1,15 @@
+export function Intro_pag_sobre(){
+    return(
+        
+        <section className="bg-gradient-to-br from-[#fffbeb] to-[#f0fdf4] py-15">
+            <div>
+                <header className="w-[350px] m-auto py-4">
+                    <h1 className="text-5xl font-bold text-center">Sobre a <span className="text-[#b45309]">CoconutStore</span></h1>
+                </header>
+                    <p className="w-[700px] m-auto text-center text-[#4B5563] py-2">
+                        Desde 2025, a CoconutStore leva para a sua mesa o sabor autêntico e a variedade dos melhores cocos frescos, selecionados diretamente da fazenda. Surgida como projeto trainee da empresa júnior Struct (UnB), nossa loja online combina qualidade, tecnologia e compromisso com a sustentabilidade para oferecer uma experiência única ao comprar diferentes tipos de coco no conforto da sua casa.
+                    </p>
+            </div>
+        </section>
+    );
+}
