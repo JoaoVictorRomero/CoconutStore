17e5042ae2cfb87592e69e18a6cc80c3a0f73585
JoaoVictorRomero <halleyloup-study@outlook.com>
2025-07-22 12:12:46 -0300

    Seed template

---
+++ prisma/seed.ts
@@ -0,0 +1,24 @@
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+async function main() {
+
+  const products = await prisma.product.createMany({
+    data: [
+      {name: 'Kit6', description: 'escreva qualquer coisal', price: 20.90},
+      {name: 'Verde Tradicional', description: 'Verde ideal para águas bem geladas', price: 20.90}
+    ],
+    // skipDuplicates: true,
+  })
+
+  console.log({products})
+
+}
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
