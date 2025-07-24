cbfa13b28b701300b069058caed0aa5d50c9ccb1
bernardoleinig <bernardoleinig@gmail.com>
2025-07-24 10:25:53 -0300

    finalizado a responsividade desse componente

--- src/app/_components/intro_pag_sobre.tsx
+++ src/app/_components/intro_pag_sobre.tsx
@@ -2,7 +2,7 @@ export function Intro_pag_sobre(){
     
     return(
         
-        <section className="bg-gradient-to-br from-[#fffbeb] to-[#f0fdf4] py-15">
+        <section className="bg-gradient-to-br from-[#fffbeb] to-[#f0fdf4] py-5">
             <div className="max-w-[1280px] m-auto">
                 <header className="w-[350px] m-auto py-4">
                     <h1 className="text-5xl font-bold text-center">Sobre a <span className="text-[#b45309]">CoconutStore</span></h1>
