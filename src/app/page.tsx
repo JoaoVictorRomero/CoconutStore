83cd3a9942b1c2e5ae35bdd5285c685656802fb8
bernardoleinig <bernardoleinig@gmail.com>
2025-07-25 23:10:03 -0300

    Refatorei o componente 'NossaHistoria' para 'BannerInformativo', tornando-o reutilizável tanto na página inicial quanto na página 'Sobre'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,32 @@
+import Link from 'next/link';
 import { Navbar } from "./_components/navbar";
 import { Footer } from "./_components/footer";
 import { Pre_footer } from "./_components/pre_footer";
 import { Intro_pag_sobre } from "./_components/intro_pag_sobre";
-import { BannerInformativo } from "./_components/BannerInformativo";
+import { BannerInformativo } from "./_components/bannerInformativo";
 
 export default function HomePage() {
   return(
     <main>
+      <Navbar/>
 
 
-      <BannerInformativo/>
+      <BannerInformativo
+          titulo = {<h1 className = "text-6xl font-semibold"> Cocos Frescos <span className="flex max-w-[300px] text-orange-700"> Direto da Fazenda</span> </h1>}
+
+          descricao = {<p className="max-w-[600px] py-10 text-justify text-[#4B5563] text-xl"> Descubra o melhor do coco em cada detalhe! Produtos selecionados com qualidade premium, frescor garantido e entrega rápida para levar até você toda a autenticidade, versatilidade e sabor que só o coco pode oferecer.</p>}
+
+          img ="logo_arrumada.png"
+
+          icone="🌴"
+          >
+              <div className="flex gap-x-3 pb-10">
+                  <button className="bg-orange-700 text-white px-6 py-3 rounded-md transition duration-150 ease-in-out hover:bg-orange-800 cursor-pointer"> <Link href="../produtos"> Compre Agora </Link> </button>
+                  <button className="bg-white text-black px-6 py-3 rounded-md border border-gray-300 hover:bg-gray-100 cursor-pointer"> <Link href= "../sobre"> Saiba Mais </Link></button>
+              </div>
+
+      </BannerInformativo>
+
       
       <Pre_footer
       tipo = "Por que Escolher a CoconutStore?"
--- src/app/sobre/page.tsx
+++ src/app/sobre/page.tsx
@@ -1,7 +1,9 @@
+import Link from 'next/link';
+
 import { Footer } from "../_components/footer";
 import { Generico_card_sobre } from "../_components/generico_card_sobre";
 import { Intro_pag_sobre } from "../_components/intro_pag_sobre";
-import { NossaHistoria } from "../_components/BannerInformativo";
+import { BannerInformativo } from "../_components/bannerInformativo";
 import { Pre_footer } from "../_components/pre_footer";
 
 export default function Page(){
@@ -11,9 +13,37 @@ export default function Page(){
             <Intro_pag_sobre/>
 
             <Generico_card_sobre/>
-            
 
-            <NossaHistoria/>
+            <BannerInformativo
+
+                titulo = {<h1 className = "text-3xl font-semibold"> Nossa História </h1>}
+
+                descricao = {<p className="w-[100%] py-3 text-justify text-[#4B5563]">
+                A CoconutStore surgiu em 2025 a partir de um projeto trainee desenvolvido para o processo seletivo da empresa júnior Struct, vinculada à Universidade de Brasília (UnB). <br /><br />
+
+                Com isso, o presente projeto foi elaborado com o objetivo de capacitar os integrantes nas tecnologias mais utilizadas no desenvolvimento web moderno, como React, TypeScript, Tailwind CSS, Next.js, entre outras. <br /><br />
+
+                Além de aprimorar habilidades técnicas, o projeto também visa promover a experiência prática em trabalho colaborativo, boas práticas de versionamento de código com Git/GitHub, e a aplicação de conceitos de design responsivo e acessibilidade, preparando os membros para os desafios do mercado de tecnologia.</p>}
+
+                img ="logo_arrumada.png" 
+                icone = "🥥" 
+                
+                >
+                    <div className="flex flex-col lg:flex-row py-8 font-bold text-sm ">
+                        <h5 className="p-2 m-auto md:m-0">Sustentável</h5>
+                        <h5 className="p-2 m-auto md:m-0">100% Natural</h5>
+                        <h5 className="p-2 m-auto md:m-0">Família</h5>
+                    </div>
+                
+            </BannerInformativo>
+
+            <Pre_footer
+            tipo = "Nossa Equipe"/>
+
+            <Pre_footer
+            tipo = "Entre em Contato"/>
+
+
 
             <Pre_footer
             tipo = "Nossa Equipe"/>
